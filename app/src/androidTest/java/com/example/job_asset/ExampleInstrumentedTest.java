88206da4b27c7f1667fc94003698de45b63471d6
Arseny802 <Arseny802@yandex.ru>
2020-12-19 13:14:47 +0300

    Tabs

--- app/src/androidTest/java/com/example/job_asset/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/job_asset/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.jobasset;
+package com.example.job_asset;
 
 import android.content.Context;
 
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.jobasset", appContext.getPackageName());
+        assertEquals("com.example.job_asset", appContext.getPackageName());
     }
 }
--- app/src/test/java/com/example/job_asset/ExampleUnitTest.java
+++ app/src/test/java/com/example/job_asset/ExampleUnitTest.java
@@ -1,4 +1,4 @@
-package com.example.jobasset;
+package com.example.job_asset;
 
 import org.junit.Test;
 
